f59084d9fadb7ddd492c35992dc435ebf1bfe313
jgularte <jack.gularte@outlook.com>
2021-01-24 15:45:23 -0800

    environments are set up and runnable

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  environment: "production",
   production: true
 };
--- src/environments/environment.sandbox.ts
+++ src/environments/environment.sandbox.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  environment: 'local',
+  environment: 'sandbox',
   production: false
 };
